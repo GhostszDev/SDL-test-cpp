aba1ea4efaf86b4afc69405cf733d084d447fd67
Steven Rivera <stevenrivera@Stevens-MBP.attlocal.net>
2019-09-10 12:10:40 -0500

    Initial commit

---
+++ Classes/Window/Window.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Steven Rivera on 9/9/19.
+//
+
+#include "Window.h"
+
+bool Window::inti() {
+    return true;
+}
+
+bool Window::release() {
+    return true;
+}
+
+Window::Window() {
+
+}
+
+Window::~Window() {
+
+}
---
+++ Classes/Window/Window.h
@@ -0,0 +1,23 @@
+//
+// Created by Steven Rivera on 9/9/19.
+//
+
+#ifndef TEST_WINDOW_H
+#define TEST_WINDOW_H
+
+#pragma once
+
+#include <cstdio>
+
+
+class Window {
+    bool inti();
+    bool release();
+
+    Window();
+    ~Window();
+
+};
+
+
+#endif //TEST_WINDOW_H
