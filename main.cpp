9d9116f555fff09bc5381ff848d2f9ea6b55d6cc
Steven Rivera <ghostszmusic@gmail.com>
2019-09-11 13:16:40 -0500

    window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <SDL.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[]) {
+
+    SDL_Window *window;
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow(
+            "Test",
+            SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            640,
+            480,
+            SDL_WINDOW_OPENGL
+            );
+
+    if(window == NULL){
+        printf("Window creation failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
